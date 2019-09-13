477dabc9377aba5eb5075dbfed8350d56ee05742
gzelinskiy <gzelinskiy@gmail.com>
2019-09-13 14:58:32 +0300

    Search contact by contactID

--- temp/testZoho.js
+++ temp/testZoho.js
@@ -1,6 +1,11 @@
 const zoho = require("../lib/zohoCRM");
 
-zoho.searchContact({ Email: "anton.shaiko9@mail.ru", Phone: "+37529" })
+/*zoho.searchContact({ Email: "anton.shaiko9@mail.ru", Phone: "+37529" })
+	.then(result => {
+		console.log(result);
+	});*/
+
+zoho.searchContactByID("3678676000069968293")
 	.then(result => {
 		console.log(result);
 	});
