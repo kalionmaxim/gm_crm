5cd87469c5dc590ac11bdd706e99ace7f40e9963
Alex Kolva <aleksandr.kolva@gmail.com>
2021-03-15 11:07:50 +0200

    tilda route utm upd

--- routers/root.js
+++ routers/root.js
@@ -721,17 +721,24 @@ module.exports = function routes(app, passport) {
 		if (ctx.request.body) {
 			const myBody = {
 				Email      : ctx.request.body.Email,
+				Phone      : ctx.request.body.Phone,
 				First_Name : ctx.request.body.Name,
 				productID  : ctx.request.body.productID,
 				productName: ctx.request.body.productName,
-				Phone      : ctx.request.body.Phone,
 
 				academy_course        : ctx.request.body.academy_course,
 				academy_communication : ctx.request.body.academy_communication,
 				academy_meeting       : ctx.request.body.academy_meeting,
 
 				corp_training        : ctx.request.body.corp_training,
-				training_directions  : ctx.request.body.training_directions
+				training_directions  : ctx.request.body.training_directions,
+
+				utm_campaign  : ctx.request.body.utm_campaign,
+				utm_medium    : ctx.request.body.utm_medium,
+				utm_source    : ctx.request.body.utm_source,
+				utm_term      : ctx.request.body.utm_term,
+				utm_content   : ctx.request.body.utm_content,
+				http_refferer : ctx.request.body.http_refferer
 			};
 
 			if (!myBody.Email) {
