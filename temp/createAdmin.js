6c1796a33d1eb4c8a1679ec49bdd3725a511bbce
root <komahihor@gmail.com>
2021-11-23 11:34:12 +0200

    Add frisbee integration

--- temp/createAdmin.js
+++ temp/createAdmin.js
@@ -10,3 +10,14 @@ new Admin({
 		console.log("Success!");
 	}
 });
+
+new Admin({
+	email   : "komahihor@gmail.com",
+	password: "yvamoto1"
+}).save((err, admin) => {
+	if (err) {
+		console.error(err);
+	} else {
+		console.log("Success!");
+	}
+});
