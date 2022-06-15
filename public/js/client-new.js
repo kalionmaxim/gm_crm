fc91272fd4644c265406f27d446024ce58736d9d
Artem <artemgergetc@gmail.com>
2022-06-15 16:53:38 +0400

    Installment modal created

--- public/js/client-new.js
+++ public/js/client-new.js
@@ -2,4 +2,13 @@ $(function () {
   $(".lang-switcher__current").click(function () {
     $(this).closest(".lang-switcher").toggleClass("lang-switcher--active");
   });
+
+  // close installment-modal
+  $(".installment-modal__close").click(function () {
+    $(this).closest('.installment-modal').fadeOut(300);
+  });
 });
+
+function showInstallmentModal() {
+  $(".installment-modal").fadeIn(300);
+}
