27d699030fe3519eccc7dbdb8068a7cb39a0d7bb
Your Name <you@example.com>
2025-07-28 16:23:53 +0300

    Change PUMB to root.js

--- routers/root.js
+++ routers/root.js
@@ -2013,8 +2013,5 @@ module.exports = function routes(app, passport) {
 		ctx.body = await Plata.processCallback(ctx.request.body);
 	});
 
-	router.post("/callback/pumb", express.json(), pumb.handlePumbCallback);
-	router.post("/callback/pumb/test", express.json(), pumb.handlePumbCallback);
-
 	app.use(router.routes());
 };
